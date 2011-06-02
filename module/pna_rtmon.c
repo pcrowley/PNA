9d3945f42bb2c2962a626d7e3c4d54b52190e716
Michael Schultz <mjschultz@gmail.com>
2011-06-02 06:10:05 -0500

    - added MCRingBuffer to the mix

--- module/pna_rtmon.c
+++ module/pna_rtmon.c
@@ -32,7 +32,10 @@ struct pna_pipedata {
     struct sk_buff *skb;        /* 8 */
     unsigned long data;         /* 8 */
 } ____cacheline_aligned;
-#define PNA_RTMON_QUEUE_SZ (2<<8)
+#define PNA_QUEUE_SZ (2<<8)
+
+#define PNA_QUEUE_BATCH_SZ (PNA_QUEUE_SZ>>2)
+#define PNA_QUEUE_NEXT(x) (((x) + 1) & (PNA_QUEUE_SZ - 1))
 
 /* PERFORMANCE */
 /* taken from linux/jiffies.h in kernel v2.6.21 */
@@ -50,7 +53,26 @@ struct pna_pipedata {
 struct pna_queue {
     int head ____cacheline_aligned;
     int tail ____cacheline_aligned;
-    struct pna_pipedata data[PNA_RTMON_QUEUE_SZ];
+    struct pna_pipedata data[PNA_QUEUE_SZ];
+};
+#elif QUEUE_TYPE == QUEUE_TYPE_MCBUF
+struct pna_queue {
+    /* shared control variables */
+    volatile int tail ____cacheline_aligned;
+    volatile int head;
+
+    /* consumer's local variables */
+    int reader_head ____cacheline_aligned;
+    int reader_next;
+    int reader_batch;
+
+    /* producer's local variables */
+    int writer_tail ____cacheline_aligned;
+    int writer_next;
+    int writer_batch;
+
+    /* buffer data */
+    struct pna_pipedata data[PNA_QUEUE_SZ];
 };
 #endif /* QUEUE_TYPE */
 
@@ -77,10 +99,10 @@ struct pna_rtmon {
     int (*pipe)(void *);
     char *name;
     struct task_struct *thread;
-#if QUEUE_TYPE == QUEUE_TYPE_CIRC
+#if (QUEUE_TYPE == QUEUE_TYPE_CIRC) || (QUEUE_TYPE == QUEUE_TYPE_MCBUF)
     struct pna_queue queue;
 #elif QUEUE_TYPE == QUEUE_TYPE_KFIFO
-    DECLARE_KFIFO(queue, struct pna_pipedata, PNA_RTMON_QUEUE_SZ);
+    DECLARE_KFIFO(queue, struct pna_pipedata, PNA_QUEUE_SZ);
 #endif /* QUEUE_TYPE */
 };
 
@@ -106,36 +128,54 @@ struct pna_rtmon monitors[] = {
 DEFINE_TIMER(clean_timer, rtmon_clean, 0, 0);
 
 /* PNA pipeline queue initialization routine */
-int pna_queue_init(struct pna_rtmon *monitor)
+void pna_queue_init(struct pna_rtmon *monitor)
 {
-#if QUEUE_TYPE == QUEUE_TYPE_CIRC
-    monitor->queue.head = monitor->queue.tail = 0;
-    memset(monitor->queue.data, 0,
-           sizeof(struct pna_pipedata)*PNA_RTMON_QUEUE_SZ);
-    return 0;
+#if (QUEUE_TYPE == QUEUE_TYPE_CIRC) || (QUEUE_TYPE == QUEUE_TYPE_MCBUF)
+    memset(&monitor->queue, 0, sizeof(struct pna_queue));
 #elif QUEUE_TYPE == QUEUE_TYPE_KFIFO
     INIT_KFIFO(monitor->queue);
-    return 0;
 #endif /* QUEUE_TYPE */
 }
 
-/* PNA queue item insertion routine */
+/**
+ * PNA queue item insertion routine
+ * @return 0 if there was no room for insertion
+ */
 int pna_enqueue(struct pna_rtmon *monitor, struct pna_pipedata *data)
 {
-#if QUEUE_TYPE == QUEUE_TYPE_CIRC
+#if QUEUE_TYPE == QUEUE_TYPE_MCBUF
+    struct pna_queue *queue = &monitor->queue;
+    int next_writer_tail = PNA_QUEUE_NEXT(queue->writer_next);
+
+    if (next_writer_tail == queue->writer_tail) {
+        if (next_writer_tail == queue->tail) {
+            return 0;
+        }
+        queue->writer_tail = queue->tail;
+    }
+    queue->data[queue->writer_next] = *data;
+    queue->writer_next = next_writer_tail;
+    queue->writer_batch++;
+    if (queue->writer_batch >= PNA_QUEUE_BATCH_SZ) {
+        queue->head = queue->writer_next;
+        queue->writer_batch = 0;
+    }
+    return 1;
+#elif QUEUE_TYPE == QUEUE_TYPE_CIRC
     /* producer */
     int next;
-    int head = monitor->queue.head;
-    int tail = ACCESS_ONCE(monitor->queue.tail);
+    struct pna_queue *queue = &monitor->queue;
+    int head = queue->head;
+    int tail = ACCESS_ONCE(queue->tail);
 
-    if (CIRC_SPACE(head, tail, PNA_RTMON_QUEUE_SZ) >= 1) {
+    if (CIRC_SPACE(head, tail, PNA_QUEUE_SZ) >= 1) {
         /* insert item into data buffer */
-        memcpy(&monitor->queue.data[head], data, sizeof(struct pna_pipedata));
+        memcpy(&queue->data[head], data, sizeof(struct pna_pipedata));
 
-        next = (head + 1) & (PNA_RTMON_QUEUE_SZ - 1);
+        next = (head + 1) & (PNA_QUEUE_SZ - 1);
         smp_wmb(); /* commit item before incrementing head */
-        monitor->queue.head = next;
-        prefetchw(&monitor->queue.data[head]);
+        queue->head = next;
+        prefetchw(&queue->data[head]);
     }
     else {
         /* nothing could be inserted */
@@ -148,24 +188,47 @@ int pna_enqueue(struct pna_rtmon *monitor, struct pna_pipedata *data)
 #endif /* QUEUE_TYPE */
 }
 
-/* PNA queue removal routine */
+/**
+ * PNA queue removal routine 
+ * @return 0 if there is nothing to dequeue
+ */
 int pna_dequeue(struct pna_rtmon *monitor, struct pna_pipedata *data)
 {
-#if QUEUE_TYPE == QUEUE_TYPE_CIRC
-    int head = ACCESS_ONCE(monitor->queue.head);
-    int tail = monitor->queue.tail;
+#if QUEUE_TYPE == QUEUE_TYPE_MCBUF
+    struct pna_queue *queue = &monitor->queue;
 
-    if (CIRC_CNT(head, tail, PNA_RTMON_QUEUE_SZ) >= 1) {
+    if (queue->reader_next == queue->reader_head) {
+        if (queue->reader_next == queue->head) {
+            return 0;
+        }
+        queue->reader_head = queue->head;
+    }
+
+    *data = queue->data[queue->reader_next];
+    queue->reader_next = PNA_QUEUE_NEXT(queue->reader_next);
+    queue->reader_batch++;
+    if (queue->reader_batch >= PNA_QUEUE_BATCH_SZ) {
+        queue->tail = queue->reader_next;
+        queue->reader_batch = 0;
+    }
+
+    return 1;
+#elif QUEUE_TYPE == QUEUE_TYPE_CIRC
+    struct pna_queue *queue = &monitor->queue;
+    int head = ACCESS_ONCE(queue->head);
+    int tail = queue->tail;
+
+    if (CIRC_CNT(head, tail, PNA_QUEUE_SZ) >= 1) {
         /* make sure we read tail before reading contents at tail */
         smp_read_barrier_depends();
 
         /* read the item from tail */
-        memcpy(data, &monitor->queue.data[tail], sizeof(struct pna_pipedata));
+        memcpy(data, &queue->data[tail], sizeof(struct pna_pipedata));
 
         /* make sure tail is read *before* moving tail */
         smp_mb();
 
-        monitor->queue.tail = (tail + 1 ) & (PNA_RTMON_QUEUE_SZ - 1);
+        queue->tail = (tail + 1 ) & (PNA_QUEUE_SZ - 1);
     }
     else {
         /* nothing to be returned */
@@ -175,22 +238,6 @@ int pna_dequeue(struct pna_rtmon *monitor, struct pna_pipedata *data)
     return 1;
 #elif QUEUE_TYPE == QUEUE_TYPE_KFIFO
     return kfifo_get(&monitor->queue, data);
-#endif /* QUEUE_TYPE */
-}
-
-/* PNA queue length routine */
-int pna_queue_len(struct pna_rtmon *monitor)
-{
-#if QUEUE_TYPE == QUEUE_TYPE_CIRC
-    int head = ACCESS_ONCE(monitor->queue.head);
-    int tail = ACCESS_ONCE(monitor->queue.tail);
-
-    return CIRC_CNT(head, tail, PNA_RTMON_QUEUE_SZ);
-#elif QUEUE_TYPE == QUEUE_TYPE_KFIFO
-    if (kfifo_is_empty(&monitor->queue)) {
-        return 0;
-    }
-    return 1;
 #endif /* QUEUE_TYPE */
 }
 
@@ -209,18 +256,11 @@ int rtmon_pipe(void *data)
 
     /* loop until we get a stop signal */
     while (!kthread_should_stop()) {
-        while (pna_queue_len(self) == 0 && !kthread_should_stop()) {
-            /* nice spin */
+        /* try to fetch data from buffer */
+        if (pna_dequeue(self, &piped) == 0) {
+            /* no work, take a break */
             schedule();
-        }
-        if (kthread_should_stop()) {
-            break;
-        }
-
-        /* fetch data from buffer */
-        ret = pna_dequeue(self, &piped);
-        if (ret == 0) {
-            pr_info("fifo underflow (%s)\n", self->name);
+            continue;
         }
 
         /* process in hook */
