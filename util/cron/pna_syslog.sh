9bb4dc64aad56d88332f1b6f2a29743dd0895d2d
Michael J. Schultz <mjschultz@gmail.com>
2011-08-04 16:24:19 -0500

    - added code to transfer to remote location

--- util/cron/pna_syslog.sh
+++ util/cron/pna_syslog.sh
@@ -2,9 +2,12 @@
 # This gets relevant pna performance data from /var/log/messages on the PNA
 # host
 
-OUTPUT_DIR="/home/pna/pna/syslogs"
+LOG_DIR="/home/pna/pna/syslogs"
 
-mkdir -p $OUTPUT_DIR
+SSH_OPTIONS="-i $BASE/.ssh/pna_rsa"
+REMOTE_LOCATION="mjschultz@mcore.arl.wustl.edu:~/syslogs"
+
+mkdir -p $LOG_DIR
 
 # Set output file date
 DATE=$(date --date='yesterday' +'%F')
@@ -19,5 +22,21 @@ LOGS="/var/log/messages.1 /var/log/messages"
 KEYWORDS="pna \(throughput\|table\|\([A-Za-z]\+[0-9]\+ \)\?rx_stats\)"
 GREP_STRING="^$LOG_DATE.*$KEYWORDS"
 
-LOG_FILE="$OUTPUT_DIR/messages_$DATE"
+LOG_FILE="$LOG_DIR/messages_$DATE"
 sudo grep -h "$GREP_STRING" $LOGS > $LOG_FILE
+
+# Attempt to copy this and any older files to remote host
+for log in $LOG_DIR/* ; do
+	# If it isn't a file, skip it
+	if [ ! -f $log ] ; then
+		continue
+	fi
+
+	# Attempt to copy to REMOTE_LOCATION
+	scp $SSH_OPTIONS $log $REMOTE_LOCATION
+
+    # If successful, remove the file
+	if [ $? -eq 0 ] ; then
+		rm -f $log
+	fi
+done
